47af83eecf6f35b0eb3e9495867f0c00dd8ffd14
MatiOrtiz <matiasnortiz.dev@gmail.com>
2024-01-13 16:08:03 -0300

    Jakarta Entity applied

--- src/main/java/com/masterspringboot/learnjpaandhibernate/course/Course.java
+++ src/main/java/com/masterspringboot/learnjpaandhibernate/course/Course.java
@@ -1,9 +1,17 @@
 package com.masterspringboot.learnjpaandhibernate.course;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Course {
 
+    @Id
     private long id;
+    @Column
     private String name;
+    @Column
     private String author;
 
     public Course(){
