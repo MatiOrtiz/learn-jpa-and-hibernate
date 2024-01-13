126b01809d1e3874ae8e45b73e69be4544d04bb8
MatiOrtiz <matiasnortiz.dev@gmail.com>
2024-01-13 16:07:01 -0300

    Renamed

--- src/main/java/com/masterspringboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/masterspringboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -1,15 +1,18 @@
-package com.masterspringboot.learnjpaandhibernate.course.jdbc;
+package com.masterspringboot.learnjpaandhibernate.course;
 
 import com.masterspringboot.learnjpaandhibernate.course.Course;
+import com.masterspringboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
+import com.masterspringboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+public class CourseCommandLineRunner implements CommandLineRunner {
 
     @Autowired
-    private CourseJdbcRepository repository;
+    //private CourseJdbcRepository repository;
+    private CourseJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
